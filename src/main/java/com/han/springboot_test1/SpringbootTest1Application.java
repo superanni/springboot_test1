b5d7325252e06e335f044c39e086d8eaa2017dfa
Axin <1148049547@qq.com>
2021-01-27 15:28:45 +0800

    dev测试

--- src/main/java/com/han/springboot_test1/SpringbootTest1Application.java
+++ src/main/java/com/han/springboot_test1/SpringbootTest1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.han.springboot_test1.dao.testMapper")
+//
 public class SpringbootTest1Application {
 
     public static void main(String[] args) {
